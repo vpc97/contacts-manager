7f6db2b60a2cfdc840d56a8eeb62ee51b7448d4b
pinalesc <pinalesc@etud.insa-toulouse.fr>
2017-11-14 14:58:03 +0100

    Create Class Java "Contact" and resolve tests from "ContactsManagerMust"

---
+++ src/main/java/com/fredericboisguerin/insa/Contact.java
@@ -0,0 +1,25 @@
+package com.fredericboisguerin.insa;
+
+public class Contact{
+    public String name;
+    public String email;
+    public String phoneNumber;
+
+    public Contact(String name, String email, String phoneNumber) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        if(phoneNumber==null){
+            return name +", " + email;
+        }else if(email==null){
+            return name + ", " + phoneNumber;
+        }else{
+            return name + ", " + email + ", " + phoneNumber;
+        }
+
+    }
+}
--- src/main/java/com/fredericboisguerin/insa/ContactsManager.java
+++ src/main/java/com/fredericboisguerin/insa/ContactsManager.java
@@ -1,16 +1,28 @@
 package com.fredericboisguerin.insa;
 
+
+import java.util.ArrayList;
+
 public class ContactsManager {
 
-    public void addContact(String name, String email, String phoneNumber) {
+    ArrayList<Contact> list = new ArrayList<Contact>();
 
+    public void addContact(String name, String email, String phoneNumber) {
+        Contact contact = new Contact(name, email, phoneNumber);
+        list.add(contact);
     }
 
     public void printAllContacts() {
-
+        for(int i=0; i<list.size();i++){
+            System.out.print(list.get(i).toString());
+        }
     }
 
     public void searchContactByName(String name) {
-
+        for(int i=0; i<list.size();i++){
+            if(list.get(i).name.toLowerCase().contains(name.toLowerCase())){
+                System.out.print(list.get(i).toString()+ "\n");
+            };
+        }
     }
 }
